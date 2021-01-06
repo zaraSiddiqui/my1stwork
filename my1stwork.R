b10adf8213bcc02f545daf30fdcd2b17e4dc5a3e
Zara Siddiqui <zara.nedian@gmail.com>
2021-01-07 03:56:57 +0500

    Alhumdullah ! after alot of effort 2nd line is printed w/o any error or using shell for config file

--- my1stwork.R
+++ my1stwork.R
@@ -4,3 +4,4 @@ print("The father of kayi tribe")
 
 print("The ultimate desire is to spread peace in the world")
 print("GO zara come on")
+print("Alhumdullah ! after alot of effort 2nd line is printed w/o any error or using shell for config file")
